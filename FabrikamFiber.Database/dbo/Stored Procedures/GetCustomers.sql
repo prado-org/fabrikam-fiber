34587753a7b1b05e7400a7bfbef54192b1059c90
Leandro Prado <leadro@microsoft.com>
2023-02-13 09:52:12 -0300

    add database project

---
+++ FabrikamFiber.Database/dbo/Stored Procedures/GetCustomers.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetCustomers]
+AS
+	SELECT
+		[ID],
+		[FirstName],
+		[LastName],
+		[Address_Street],
+		[Address_City],
+		[Address_State],
+		[Address_Zip] 
+	FROM 
+		dbo.Customers
+	ORDER BY 
+		ID
